b8b78d54f89a6194250a14e65ff31fe325c1806f
Mauricio Meléndez <mauhylian@gmail.com>
2021-02-24 15:47:10 -0600

    Updated ScanActivity.kt and activity_scan.xml

--- app/src/main/java/com/example/practice/ScanActivity.kt
+++ app/src/main/java/com/example/practice/ScanActivity.kt
@@ -1,5 +1,7 @@
 package com.example.practice
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
@@ -8,6 +10,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.zxing.integration.android.IntentIntegrator
 
 import kotlinx.android.synthetic.main.activity_scan.* // Para que detecte el drawerLayout id
 
@@ -18,11 +21,6 @@ class ScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-
-        findViewById<Button>(R.id.logOutBtn).setOnClickListener {
-            Firebase.auth.signOut()
-            finish()
-        }
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close )
         drawerLayout.addDrawerListener(toggle)
@@ -44,6 +42,12 @@ class ScanActivity : AppCompatActivity() {
             }
             true
         }
+
+        findViewById<Button>(R.id.scanBtn).setOnClickListener {
+            val scanner = IntentIntegrator(this)
+            scanner.initiateScan()
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -53,4 +57,18 @@ class ScanActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK) {
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if(result != null) {
+                if(result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Scanned: ${result.contents}", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
 }
